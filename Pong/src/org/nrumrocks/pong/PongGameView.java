fd572427a045aeb0c6dfca95d2fa29bed9916e94
stefantouhey <stefan2e@earthlink.net>
2013-09-12 10:23:37 -0700

    Paddle first appearance

--- Pong/src/org/nrumrocks/pong/PongGameView.java
+++ Pong/src/org/nrumrocks/pong/PongGameView.java
@@ -4,12 +4,21 @@ import jgame.GContainer;
 
 public class PongGameView extends GContainer {
 	
+	
 	public PongGameView () {
-	    // Set the size.
+		// Set the size.
 	    setSize(640, 480);
 
-	    // Create and add a paddle.
-	    add(new PongPaddle());
+	    // Create a paddle to add.
+	    PongPaddle paddle = new PongPaddle();
+
+	    // Add the paddle to the game view.
+	    add(paddle);
+	    
+	 // Set the paddle's location.
+	    paddle.setLocation(50, 480 / 2);
+	    
+	    
 	}
 
 }
--- Pong/src/org/nrumrocks/pong/PongPaddle.java
+++ Pong/src/org/nrumrocks/pong/PongPaddle.java
@@ -1,15 +1,13 @@
 package org.nrumrocks.pong;
 
 import jgame.ImageCache;
-import java.awt.Image;
 import jgame.GSprite;
 
 
 public class PongPaddle extends GSprite {
-	{
-		
-		ImageCache cache = ImageCache.forClass(Pong.class);
-		Image paddle = cache.get("paddle.png");
-		super(paddle);
-}
+	
+	public PongPaddle () {
+		super(ImageCache.forClass(Pong.class).get("paddle.png"));
+	}
+	
 }
